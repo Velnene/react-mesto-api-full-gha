fd9c8570de072a26c89763c7e088ca9cbe18631c
Viktor <viktor.hidirov@yandex.ru>
2023-05-24 20:54:15 +0700

    вернул логер и корс

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('cors');
 const { errors } = require('celebrate');
 const { handleNotFoundUrl } = require('./errors/handleNotFoundUrl');
 const { login, createUser } = require('./controllers/user');
 const { loginValidate, createValidate } = require('./errors/userError');
 
 const app = express();
+app.use(cors({
+  origin: ['https://web-15.viktor5211.nomoredomains.monster',
+    'https://web-15.viktor5211.nomoredomains.monster/'],
+  credentials: true,
+}));
 const {
   PORT = 3000,
   MONGO_URL = 'mongodb://0.0.0.0:27017/mestodb',
@@ -13,14 +20,22 @@ const {
 
 const { userRouter, cardRouter } = require('./routes');
 
+app.use(requestLogger);
 app.use(express.json());
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', loginValidate, login);
 app.post('/signup', createValidate, createUser);
-app.use(userRouter);
-app.use(cardRouter);
+app.use('/api', userRouter);
+app.use('/api', cardRouter);
 app.patch('*', (req, res) => {
   handleNotFoundUrl(req, res);
 });
+
+app.use(errorLogger);
 app.use(errors());
 
 mongoose.connect(MONGO_URL, {});
