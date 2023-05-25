b5e850ad2194d5c95b66c0ecbd9f7c349ed0a8e6
Viktor <viktor.hidirov@yandex.ru>
2023-05-25 22:28:59 +0700

    -

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -38,7 +38,7 @@ function App() {
         .then((res) => {
           if (res) {
             setLoggedIn(true);
-            setEmail(res.data.email);
+            setEmail(res.data?.email);
             navigate('/', { replace: true });
           }
         })
