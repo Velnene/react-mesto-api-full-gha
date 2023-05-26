369d7ac84d4787c0bef3e13e6097361bc3a1f2ef
Viktor <viktor.hidirov@yandex.ru>
2023-05-26 17:32:31 +0700

    errorHandler добавил

---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,8 @@
+const errorHandler = (err, req, res, next) => {
+  const status = err.statusCode || 500;
+  const message = err.message || 'На сервере произошла ошибка.';
+  res.status(status).send({ err, message });
+  next();
+};
+
+module.exports = errorHandler;
