8b6de3c5d34f807df01c78f54efff5fdc480fbc4
Viktor <viktor.hidirov@yandex.ru>
2023-05-27 14:10:13 +0700

    откатил

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -89,13 +89,27 @@ const updateUserAvatar = (req, res, next) => {
     });
 };
 
-const login = (req, res, next) => {
-  const { email, password } = req.body;
+// const login = (req, res, next) => {
+//   const { email, password } = req.body;
 
-  User.findUserByCredentials(email, password)
+//   User.findUserByCredentials(email, password)
+//     .then((user) => {
+//       const token = generateToken({ _id: user.id });
+//       res.status(OK).send({ token });
+//     })
+//     .catch(next);
+// };
+
+const login = (req, res, next) => {
+  const { email } = req.body;
+  User.findOne({ email })
+    .select('+password')
     .then((user) => {
+      if (!user) {
+        next(new UnauthorizedError('Неправильные почта или пароль'));
+      }
       const token = generateToken({ _id: user.id });
-      res.status(OK).send({ token });
+      return res.send({ token });
     })
     .catch(next);
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const isEmail = require('validator/lib/isEmail');
 const urlValid = require('../utils/urlValid');
-const UnauthorizedError = require('../errors/UnauthorizedError');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,20 +41,20 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne({ email })
-    .then((user) => {
-      if (!user) {
-        throw new UnauthorizedError('Неправильные почта или пароль1');
-      }
-      return bcrypt.compare(password, user.password)
-        .then((matched) => {
-          if (!matched) {
-            throw new UnauthorizedError('Неправильные почта или пароль2');
-          }
-          return user;
-        });
-    });
-};
+// userSchema.statics.findUserByCredentials = function (email, password) {
+//   return this.findOne({ email })
+//     .then((user) => {
+//       if (!user) {
+//         throw new UnauthorizedError('Неправильные почта или пароль1');
+//       }
+//       return bcrypt.compare(password, user.password)
+//         .then((matched) => {
+//           if (!matched) {
+//             throw new UnauthorizedError('Неправильные почта или пароль2');
+//           }
+//           return user;
+//         });
+//     });
+// };
 
 module.exports = mongoose.model('user', userSchema);
