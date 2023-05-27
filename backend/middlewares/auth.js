8a56881ad60563abd482e35b4089094a924ba8f3
Viktor <viktor.hidirov@yandex.ru>
2023-05-27 12:12:17 +0700

    -

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ const auth = (req, res, next) => {
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     // next(new UnauthorizedError('Неправильные почта или пароль3'));
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация3' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
@@ -16,7 +16,7 @@ const auth = (req, res, next) => {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
     // next(new UnauthorizedError('Неправильные почта или пароль4'));
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    return res.status(401).send({ message: 'Необходима авторизация4' });
   }
 
   req.user = payload;
