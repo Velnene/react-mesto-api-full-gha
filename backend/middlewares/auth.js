b40ead3004cc3ba033cfe48ffe2e20d410dfdcf1
Viktor <viktor.hidirov@yandex.ru>
2023-05-27 11:57:43 +0700

    откат до рабочего состояния

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,13 @@
 const { SECRET_KEY = 'some-secret-key' } = process.env;
 const jwt = require('jsonwebtoken');
-const UnauthorizedError = require('../errors/UnauthorizedError');
+// const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Неправильные почта или пароль3'));
+    // next(new UnauthorizedError('Неправильные почта или пароль3'));
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
@@ -14,7 +15,8 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    next(new UnauthorizedError('Неправильные почта или пароль4'));
+    // next(new UnauthorizedError('Неправильные почта или пароль4'));
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   req.user = payload;
